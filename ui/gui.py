2b30c630f5f0daaeca3e7224f43f09d806bba7c8
SafaObuz <sfaboyeo@gmail.com>
2024-04-13 19:07:09 -0400

    animating text

--- ui/gui.py
+++ ui/gui.py
@@ -9,11 +9,22 @@ font = pygame.font.Font('ui/font/Quicksand-Medium.ttf', 50)
 #sound = pygame.mixer.Sound('audio/fdsfds.wav') Maybe use this method if we need
 
 top_text = Text(font)
+mid_text = Text(font)
+bot_text = Text(font)
+
 
 def start_ui():
     top_text.set_text("testing")
     top_text.move_text_to(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/10)
-    top_text.set_text_vel(0, 50)
+
+    # mid_text.set_text("testing")
+    # mid_text.move_text_to(DISPLAY_WIDTH/2, DISPLAY_HEIGHT*(9/10))
+    # mid_text.slide_text(DISPLAY_WIDTH*(6/7), DISPLAY_HEIGHT*(4/10))
+
+    # bot_text.set_text("testing")
+    # bot_text.move_text_to(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2)
+    # bot_text.slide_text(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/10)
+
     global event_cnt
     event_cnt = 0
 
@@ -21,16 +32,16 @@ def start_ui():
 
 def ui_loop():
     global event_cnt
-    dt = clock.tick(60) / 1000
+    dt = clock.tick(60) / 1000 #units are seconds
     #print(f'DT:{dt:0.10f}')
 
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
             event_cnt += 1
             if event_cnt == 1:
-                top_text.set_text_vel(0, 0)
+                top_text.slide_text(DISPLAY_WIDTH/2, DISPLAY_HEIGHT*(9/10))
             if event_cnt == 2:
-                top_text.set_text("new message!")
+                top_text.animate_text("new message!")
             if event_cnt >= 3:
                 pygame.quit()
                 return False
--- ui/text.py
+++ ui/text.py
@@ -4,18 +4,29 @@ from globals import *
 class Text:
     def __init__(self, font, color = DEFAULT_TEXT_COLOR, bg = DEFAULT_BG_COLOR):
         self._message = ''
+        self._next_message = ''
+        self._state = "static"
         self._font = font
         self._antialias = True
         self._color = color
         self._bg = bg #make sure the background color matches for fast AA, hence globals
         self._x : float = 0
         self._y : float = 0
+        self._next_x : float = 0
+        self._next_Y : float = 0
         self._dx : float = 0
         self._dy : float = 0
+        self._time_sum = 0
 
     def move_text_to(self, x, y):
         self._x = x
         self._y = y
+        self._next_x = x
+        self._next_y = y
+    
+    def slide_text(self, x, y):
+        self._next_x = x
+        self._next_y = y
     
     def set_text_vel(self, x, y):
         self._dx = x
@@ -23,6 +34,19 @@ class Text:
 
     def set_text(self, message):
         self._message = message
+        self._next_message = message
+        self._state = "static"
+
+    def animate_text(self, message, animation_type = "left"):
+        if animation_type == "left":
+            self._next_message = message
+            self._state = "left_clear"
+        if animation_type == "right":
+            self._next_message = message
+            self._state = "right_clear"
+    
+    def is_animating(self):
+        return self._state == 'static'
     
     def display_text(self):
         text_surface = self._font.render(self._message, self._antialias, self._color, self._bg) #bg is supplied for fast aa
@@ -32,6 +56,36 @@ class Text:
     def update(self, dt):
         self._x += self._dx * dt
         self._y += self._dy * dt
+        self._time_sum += dt
+        print("vals", self._y, self._next_y)
+
+
+        if self._next_x != self._x or self._next_y != self._y:
+            self.set_text_vel(self._next_x - self._x, self._next_y - self._y)
+            if abs(self._next_x - self._x) < 5:
+                self.move_text_to(self._next_x, self._y)
+            if abs(self._next_y - self._y) < 5:
+                self.move_text_to(self._x, self._next_y)
+
+        if self._time_sum >= ANIMATION_SPEED:
+            self._time_sum = 0
+            if self._message != self._next_message:
+                if self._state == "right_clear":
+                    self._message = self._message[:len(self._message) - 1]
+                    if self._message == '':
+                        self._state = "right_load"
+                if self._state == "right_load":
+                    self._message = self._next_message[:len(self._message) + 1]
+                if self._state == "left_clear":
+                    self._message = self._message[1:len(self._message)]
+                    if self._message == '':
+                        self._state = "left_load"
+                if self._state == "left_load":
+                    self._message = self._next_message[len(self._next_message) - len(self._message) - 1:len(self._next_message)]
+
+            if self._message == self._next_message:
+                self._state = "static"
+
 
         self.display_text()
 
