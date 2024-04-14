a33527409ea0509f8799d3618cc51a29d3d088a0
SafaObuz <sfaboyeo@gmail.com>
2024-04-13 22:12:24 -0400

    text finished with alpha, now moving graphics

--- ui/gui.py
+++ ui/gui.py
@@ -12,23 +12,25 @@ top_text = Text(font)
 mid_text = Text(font)
 bot_text = Text(font)
 
+eye = pygame.image.load('ui/graphics/eye.png').convert_alpha()
+eye_rect = eye.get_rect()
+arrow = pygame.image.load('ui/graphics/arrow.jpg').convert_alpha()
 
 def start_ui():
-    top_text.set_text("testing")
-    top_text.move_text_to(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/10)
+    top_text.set_text("t1")
+    top_text.move_text_to(DISPLAY_WIDTH*(5/10), DISPLAY_HEIGHT*(1/10))
+    top_text.slide_text(DISPLAY_WIDTH*(20/10), DISPLAY_HEIGHT*(20/10))
 
-    # mid_text.set_text("testing")
-    # mid_text.move_text_to(DISPLAY_WIDTH/2, DISPLAY_HEIGHT*(9/10))
-    # mid_text.slide_text(DISPLAY_WIDTH*(6/7), DISPLAY_HEIGHT*(4/10))
+    mid_text.set_text("lalalaalalalla")
+    mid_text.move_text_to(DISPLAY_WIDTH*(2/10), DISPLAY_HEIGHT*(8/10))
+    mid_text.slide_text(DISPLAY_WIDTH*(8/10), DISPLAY_HEIGHT*(2/10))
 
-    # bot_text.set_text("testing")
-    # bot_text.move_text_to(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/2)
-    # bot_text.slide_text(DISPLAY_WIDTH/2, DISPLAY_HEIGHT/10)
+    bot_text.set_text("lalalalalalaal")
+    bot_text.move_text_to(DISPLAY_WIDTH*(4/10), DISPLAY_HEIGHT*(6/10))
+    bot_text.slide_text(DISPLAY_WIDTH*(6/10), DISPLAY_HEIGHT*(4/10))
 
     global event_cnt
     event_cnt = 0
-
-
 
 def ui_loop():
     global event_cnt
@@ -39,9 +41,11 @@ def ui_loop():
         if event.type == pygame.MOUSEBUTTONDOWN:
             event_cnt += 1
             if event_cnt == 1:
-                top_text.slide_text(DISPLAY_WIDTH/2, DISPLAY_HEIGHT*(9/10))
+                pass
             if event_cnt == 2:
                 top_text.animate_text("new message!")
+                mid_text.animate_text("konglonglonglong", animation_type = "right")
+                bot_text.animate_text("xxxxxxxxxxxxxxxx", animation_type = "right")
             if event_cnt >= 3:
                 pygame.quit()
                 return False
@@ -52,6 +56,12 @@ def ui_loop():
     screen.fill(DEFAULT_BG_COLOR)
 
     top_text.update(dt)
+    mid_text.update(dt)
+    bot_text.update(dt)
+    eye_rect.center = (200, 200)
+    screen.blit(eye, eye_rect)
+    screen.blit(arrow, (200,200))
+
 
     pygame.display.flip()
 
--- ui/text.py
+++ ui/text.py
@@ -1,6 +1,8 @@
 import pygame
 from globals import *
 
+MIN_TEXT_SPEED = 100
+
 class Text:
     def __init__(self, font, color = DEFAULT_TEXT_COLOR, bg = DEFAULT_BG_COLOR):
         self._message = ''
@@ -9,6 +11,7 @@ class Text:
         self._font = font
         self._antialias = True
         self._color = color
+        self._alpha = 100
         self._bg = bg #make sure the background color matches for fast AA, hence globals
         self._x : float = 0
         self._y : float = 0
@@ -51,21 +54,31 @@ class Text:
     def display_text(self):
         text_surface = self._font.render(self._message, self._antialias, self._color, self._bg) #bg is supplied for fast aa
         text_rectangle = text_surface.get_rect(center = (self._x, self._y))
+        text_surface.set_alpha(self._alpha)
+        text_surface.set_colorkey(DEFAULT_BG_COLOR)
         pygame.display.get_surface().blit(text_surface, text_rectangle)
 
     def update(self, dt):
         self._x += self._dx * dt
         self._y += self._dy * dt
         self._time_sum += dt
-        print("vals", self._y, self._next_y)
-
+        
+        def sgn(x):
+            return (x > 0) - (x < 0)
 
         if self._next_x != self._x or self._next_y != self._y:
-            self.set_text_vel(self._next_x - self._x, self._next_y - self._y)
-            if abs(self._next_x - self._x) < 5:
-                self.move_text_to(self._next_x, self._y)
-            if abs(self._next_y - self._y) < 5:
-                self.move_text_to(self._x, self._next_y)
+            x = max(self._next_x - self._x, MIN_TEXT_SPEED*sgn(self._next_x - self._x), key=abs)
+            y = max(self._next_y - self._y, MIN_TEXT_SPEED*sgn(self._next_y - self._y), key=abs)
+            x = min(self._next_x - self._x, MAX_TEXT_SPEED*sgn(self._next_x - self._x), key=abs)
+            y = min(self._next_y - self._y, MAX_TEXT_SPEED*sgn(self._next_y - self._y), key=abs)
+            self.set_text_vel(x, y)
+            if abs(self._next_x - self._x) < 0.9:
+                self._x = self._next_x
+                self._dx = 0
+            if abs(self._next_y - self._y) < 0.9:
+                self._y = self._next_y
+                self._dy = 0
+
 
         if self._time_sum >= ANIMATION_SPEED:
             self._time_sum = 0
